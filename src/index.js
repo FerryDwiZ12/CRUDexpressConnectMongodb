4c7cdbcbb2936c8955a0bacf730ed554f1212e10
Ferry DwiZulkhifli <73521546+FerryDwiZ12@users.noreply.github.com>
2023-07-23 00:50:42 +0700

    grouping routes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
+// index.js
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const app = express();
-const userRoutes = require('./routes/user.routes')
-
+const userRoutes = require("./routes/user.routes");
 
 const PORT = process.env.PORT || 3001;
 
 dotenv.config();
-
 connectDB();
 
-app.use(bodyParser.json())
-
-app.use(bodyParser.urlencoded({
-  extended : true,
-}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -24,11 +20,8 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 
-app.post('/addUser', userRoutes )
-app.get('/user', userRoutes)
-app.get('/user/:id', userRoutes)
-app.delete('/delete/:id', userRoutes)
-app.put('/update/:id', userRoutes)
+// Menggunakan grup rute untuk pengguna (users)
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,67 +1,67 @@
-const express = require('express')
-const router = express.Router()
-const users = require('../models/users')
+const express = require("express");
+const router = express.Router();
+const users = require("../models/users");
 
-router.post('/addUser', (req, res) => {
-    const user = new users({
-        nama : req.body.nama,
-        umur : req.body.umur,
-        email : req.body.email,
-        password : req.body.password,
-    })
+// Route untuk menambahkan user baru
+router.post("/", async (req, res) => {
+  const user = new users({
+    nama: req.body.nama,
+    umur: req.body.umur,
+    email: req.body.email,
+    password: req.body.password,
+  });
 
-    try {
-        const usersToSave = user.save()
-        res.status(200).json(usersToSave)
-    } catch (error) {
-        res.status(400).json({message: error.message})
-    }
-})
+  try {
+    const usersToSave = await user.save();
+    res.status(200).json(usersToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-router.get('/user', async (req, res)=>{
-    try {
-        const user = await users.find()
-        res.json(user)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+// Route untuk mendapatkan semua user
+router.get("/", async (req, res) => {
+  try {
+    const user = await users.find();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.get('/user/:id', async (req, res)=> {
-    try {
-        const user = await users.findById(req.params.id)
-        res.json(user)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
+// Route untuk mendapatkan detail user berdasarkan ID
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await users.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+// Route untuk menghapus user berdasarkan ID
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteUserData = await users.findByIdAndDelete(id);
+    res.send(`user with ${deleteUserData.nama} has been deleted.`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-router.delete('/delete/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const deleteUserData = await users.findByIdAndDelete(id)
-        res.send(`user with ${deleteUserData.nama} has been delected..`)
-    } catch (error) {
-        res.status(400).json({message: error.message})
-    }
-})
+// Route untuk memperbarui informasi user berdasarkan ID
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateUserData = req.body;
+    const options = { new: true };
 
-
-router.put('/update/:id', async (req, res)=> {
-    try {
-        const id = req.params.id
-        const updateUserData = req.body;
-        const options = {new: true};
-
-        const result = await users.findByIdAndUpdate(
-            id, updateUserData, options
-        )
-
-        res.send(result)
-    } catch (error) {
-        res.status(400).json({message: error.message})
-    }
-})
+    const result = await users.findByIdAndUpdate(id, updateUserData, options);
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
